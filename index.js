b62d1295fca4fe2230ca3049486b23eee409605e
cocadmin <thomas@cocadmin.com>
2022-02-01 17:54:59 -0500

    looping in console

---
+++ index.js
@@ -0,0 +1,25 @@
+const BTCMiner = require('bitcoin-miner');
+// View this block in Block Explorer:  https://insight.bitpay.com/block/00000000000000000020cf2bdc6563fb25c424af588d5fb7223461e72715e4a9
+// Get it in JSON format: https://insight.bitpay.com/api/block/00000000000000000020cf2bdc6563fb25c424af588d5fb7223461e72715e4a9
+const block = {
+    version: 536870912,
+    previousblockhash: '00000000000000000061abcd4f51d81ddba5498cff67fed44b287de0990b7266',
+    merkleroot: '871148c57dad60c0cde483233b099daa3e6492a91c13b337a5413a4c4f842978',
+    time: 1515252561,
+    bits: '180091c1'
+};
+
+const miner = new BTCMiner(block);
+let hash;
+let found = false;
+
+let nonce = 35291998 // initial nonce
+while (!found) { 
+    hash = miner.getHash(nonce);
+    //console.log(hash);
+    found = miner.checkHash(hash);
+    if (found) {
+		miner.verifyNonce(block, nonce);
+	}
+    nonce++;
+}
