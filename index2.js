510002f3eaf629cb85cd83c90c9a0c144be4f859
cocadmin <thomas@cocadmin.com>
2022-02-09 19:30:00 -0500

    working worker + fps

--- index2.js
+++ index2.js
@@ -1,5 +1,24 @@
-var myWorker = new Worker('worker.js');
+var myWorker = new Worker('worker-bundle.js');
+console.log("starting worker");
+
+let invocations = 0 ;
+
 myWorker.addEventListener('message', function(e) {
-    console.log('msg from wrkr: ', e.data);
+    document.getElementById('hash').textContent = e.data;
+    invocations++;
 }, false);
-myWorker.postMessage(123);
+
+var intervalId = window.setInterval(function(){
+    document.getElementById('rate').textContent = (invocations * 10 * 50) + " h/s"; //invoc * 10fps * worker evey 100 hash
+    invocations = 0;
+  }, 100);
+
+const block = {
+    version: 536870912,
+    previousblockhash: '00000000000000000061abcd4f51d81ddba5498cff67fed44b287de0990b7266',
+    merkleroot: '871148c57dad60c0cde483233b099daa3e6492a91c13b337a5413a4c4f842978',
+    time: 1515252561,
+    bits: '180091c1'
+};
+message = {block : block , startNonce : 44500998}
+myWorker.postMessage(message);
--- worker.js
+++ worker.js
@@ -1,10 +1,26 @@
-self.addEventListener('message', function(e) {
-    var i = 0;
-    while(i < 1000){
-        i++
-        if (i%10 == 0 ){
-            self.postMessage(i);
+const BTCMiner = require('bitcoin-miner');
+
+self.addEventListener('message', function(message) {
+    console.log("started worker",message);
+    block = message.data.block;
+    nonce = message.data.startNonce;
+    const miner = new BTCMiner(block);
+    let hash;
+    let found = false;
+
+    while (!found) { 
+        hash = miner.getHash(nonce);
+        if (nonce%50 == 0 ){
+            self.postMessage(hash.toString('hex'));
         }
+        found = miner.checkHash(hash);
+        if (found) {
+            miner.verifyNonce(block, nonce);
+            console.log("found!!!!!!!!");
+            self.close();
+
+        }
+        nonce++;
     }
     self.close();
 }, false);
